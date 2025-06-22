7d841d70140e4b7d2201b3804584fa05d836da5a
Patrick <lusielar@naver.com>
2025-06-22 20:38:55 +0900

    :rocket: feat: 초기 프로젝트 설정
    1. Gradle 프로젝트 구성
       - Gradle Wrapper 추가 및 설정 (gradle-wrapper.properties)
       - build.gradle 및 settings.gradle 파일 생성
    
    2. Spring Boot 애플리케이션 설정
       - PaymentApplication 클래스 및 Test 생성
       - application-dev.yml 환경설정 추가
    
    3. 로깅 설정 추가
       - logback-spring.xml 파일 생성
    
    4. MDC 로깅 필터 추가
       - MdcLoggingFilter 구현
    
    5. 기타 파일 추가
       - .gitignore 및 .gitattributes 파일 추가

---
+++ src/main/java/com/pg/payment/util/filter/MdcLoggingFilter.java
@@ -0,0 +1,46 @@
+package com.pg.payment.util.filter;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class MdcLoggingFilter extends OncePerRequestFilter {
+    
+    private static final String TRACE_ID_KEY = "traceId";
+    private static final String URI_KEY = "requestURI";
+    private static final String METHOD_KEY = "requestMethod";
+    
+    
+    @Override
+    protected void doFilterInternal(@Nonnull HttpServletRequest request,
+                                    @Nonnull HttpServletResponse response,
+                                    @Nonnull FilterChain filterChain)
+            throws IOException, jakarta.servlet.ServletException {
+        
+        try {
+            String traceId = generateTraceId();
+            MDC.put(TRACE_ID_KEY, traceId);
+            MDC.put(URI_KEY, request.getRequestURI());
+            MDC.put(METHOD_KEY, request.getMethod());
+            
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+    
+    private String generateTraceId() {
+        long time = System.currentTimeMillis();
+        int rand = ThreadLocalRandom.current().nextInt(1000, 9999);
+        return time + "-" + rand;
+    }
+    
+}
